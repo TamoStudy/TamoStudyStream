2540575ce3b5585b2ca4a83439b23ea0512ce781
Anthony <59890724+narlock@users.noreply.github.com>
2023-01-07 09:32:33 -0600

    Moving TamoStudyStream to this repo

---
+++ src/TamoStream.java
@@ -0,0 +1,11 @@
+/**
+ * TamoStream
+ * @author Anthony Narlock (narlock)
+ * @brief Runs the Main loop of the project
+ */
+
+public class TamoStream {
+	public static void main(String[] args) {
+		MainGUI gui = new MainGUI();
+	}
+}
